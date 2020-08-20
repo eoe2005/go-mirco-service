0ec2a798e1f949f017c391d7c1fd2192eb34365b
耿鸿飞 <genghongfei@soyoung.com>
2020-08-20 15:29:27 +0800

    fx

--- conf/conf.go
+++ conf/conf.go
@@ -3,14 +3,16 @@ package conf
 import (
 	"fmt"
 	"net"
+	"strings"
 	"sync"
+	"time"
 
 	"github.com/eoe2005/go-mirco-service/log"
 )
 
 var (
 	//服务器保存的服务注册内容
-	serverService map[string]map[string]int
+	serverService map[string]map[string]int64
 	// 服务配置保持的额锁
 	serverServiceLock sync.Mutex
 	// 服务器保存的配置数据
@@ -77,6 +79,14 @@ func serverProcessClient(con *net.TCPConn) {
 
 }
 
+// 发送数据给客户端
+func sendClient(cmd, data string) {
+	line := fmt.Sprintf("%s|||%s\n", cmd, data)
+	for con, _ := range clientConMap {
+		con.Write([]byte(line))
+	}
+}
+
 // udp运行，为了服务注册
 func udpRun(port int) {
 	defer mainRun.Done()
@@ -91,6 +101,7 @@ func udpRun(port int) {
 		log.Debug("注册中心配置启动失败 -> 绑定端口 %s", addrString)
 	}
 	defer updcon.Close()
+	go releaseServerHost()
 	for isRun {
 		buf := make([]byte, 1024)
 		len, addr, _ := updcon.ReadFromUDP(buf)
@@ -104,10 +115,36 @@ func udpRun(port int) {
 func registerServerHost(addr *net.UDPAddr, data []byte) {
 	serverServiceLock.Lock()
 	defer serverServiceLock.Unlock()
-	addr.IP.String()
+	appNamePort := strings.Split(string(data), ":")
+	appName := appNamePort[0]
+	port := appNamePort[1]
+
+	host := fmt.Sprintf("%s:%s", addr.IP, port)
+
+	v, ok := serverService[appName]
+	if !ok {
+		v = map[string]int64{}
+		serverService[appName] = v
+	}
+	v[host] = time.Now().Unix()
+	sendClient("as", fmt.Sprintf("%s:%s", appName, host))
 }
 
 // 定期删除掉掉线的服务
 func releaseServerHost() {
-	//time.Ticker
+	timeTicker := time.NewTicker(time.Second * 10)
+	for {
+		timeN := time.Now().Unix()
+		serverServiceLock.Lock()
+		for app, v := range serverService {
+			for host, t := range v {
+				if timeN-t > 10 {
+					sendClient("ds", fmt.Sprintf("%s:%s", app, host))
+					delete(v, host)
+				}
+			}
+		}
+		serverServiceLock.Unlock()
+		<-timeTicker.C
+	}
 }
